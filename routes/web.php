40b81080ef9989de33a545a3c2da78dbd918146d
Herman <herman.papko@gmail.com>
2025-07-04 22:29:17 +0200

    learned about submiting form to a database and valaues validation

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExampleController;
+use App\Http\Controllers\UserController;
 
 
 Route::get('/', [ExampleController::class, 'homePage']);
 
 Route::get('/about', [ExampleController::class, 'aboutPage']);
+
+Route::post('/register', [UserController::class, 'register']);
